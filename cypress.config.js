5fff60a4693c805c375deedc6f6314e154f3f516
mateunix <mateusagre@yandex.com>
2023-09-18 22:30:12 -0300

    adjusting configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://mateusagredevweb.netlify.app/',
+    baseUrl: 'https://agredevops.netlify.app/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
