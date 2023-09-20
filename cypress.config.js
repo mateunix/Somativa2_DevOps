721437a23bef32416331deae137fb72dc107510a
mateunix <mateusagre@yandex.com>
2023-09-20 17:25:51 -0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://agredevops.netlify.app/',
+    baseUrl: 'https://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
